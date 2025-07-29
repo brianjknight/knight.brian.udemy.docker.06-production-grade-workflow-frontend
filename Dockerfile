c2a1a99b83d92d2065f9a2d63c1a58479c3b42de
knightjbrian <knight.j.brian@gmail.com>
2025-07-29 15:11:46 -0600

    added config for AWS Elastic Beanstalk deployment

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,5 @@ RUN npm run build
 
 # Build nginx container
 FROM nginx
-
+EXPOSE 80
 COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>New tag content!</p>
+        <p>New tag content added!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
